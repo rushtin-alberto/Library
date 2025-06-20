3728a192cf177dbe05d24794a7e85968bf7ce26e
Rushtin Alberto <RUSHTIN.SULAYBAR.ALBERTO@ibm.com>
2025-06-20 12:14:30 +0800

    Added book addition feature

---
+++ src/Library/Library.java
@@ -0,0 +1,17 @@
+package Library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<String> books = new ArrayList<>();
+
+    public void addBook(String book) {
+        books.add(book);
+        System.out.println("Book added: " + book);
+    }
+
+    public List<String> getBooks() {
+        return books;
+    }
+}
